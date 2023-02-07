790f1b079166b7600c4320f4be44f772941fc9a6
Mark Miro <contact@markmiro.com>
2023-02-07 12:26:50 -0800

    Update copy and links

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import { config } from "../stuff/config";
 import { ConvertKitForm } from "../stuff/ConvertKitForm";
+
+const fullListUrl =
+  "https://www.notion.so/markmiro/10x-Science-cbe517733fa4452aaf6a9f2e2042f729";
+
+const contributeUrl =
+  "https://www.notion.so/markmiro/How-to-contribute-a-study-ba6854897bef443d9643fcd69e254e04";
 
 export default function IndexPage() {
   return (
     <div>
       <h1>10x Science</h1>
-      <p>Science with 10x results:</p>
+      <p>Some results:</p>
       <ul>
         <li>
           People who smoke cigarettes are{" "}
@@ -13,6 +18,14 @@ export default function IndexPage() {
             15-30x
           </a>{" "}
           more likely to get lung cancer.
+        </li>
+        <li>
+          "The quantity of prions in the brain of a patient who died of
+          Alzheimer's at 40 were on average{" "}
+          <a href="https://www.ucsf.edu/news/2019/05/414326/alzheimers-disease-double-prion-disorder-study-shows#:~:text=were%20on%20average-,32%20times,-higher%20than%20in">
+            32
+          </a>{" "}
+          times higher than in a patient who died at 90."
         </li>
         <li>
           Child sexual abuse goes up{" "}
@@ -35,49 +48,62 @@ export default function IndexPage() {
           higher than it is.
         </li>
         <li>
-          <a
-            href={config.submitStudyUrl}
-            style={{
-              display: "inline-block",
-              color: "white",
-              background: "var(--blue-dark)",
-              padding: "6px 12px",
-              textDecoration: "none",
-            }}
-          >
-            Submit a study ↗
-          </a>
+          Professors are{" "}
+          <a href="https://twitter.com/knrd_z/status/1572092216788815872">
+            12-25
+          </a>{" "}
+          times more likely to have a parent with a PhD than the general
+          population.
+        </li>
+        <li>
+          Ants occupy similar biomass to humans and consume{" "}
+          <a href="https://medium.com/collective-wisdom/the-talebian-and-thielian-moment-af14d7016adb">
+            10x
+          </a>{" "}
+          the amount of food as humans, compared to their body weight.
+        </li>
+        <li>
+          <a href={fullListUrl}>See the full list on Notion ↗</a>
         </li>
       </ul>
+      <p>
+        Imagine if this list got infinitely longer. Imagine using it to cut
+        through the noise of the information onslaught we're used to. This is
+        why the website exists.
+      </p>
       <hr />
-      <h3>What counts as 10x</h3>
+      <a href={contributeUrl}>
+        See how to start contibuting your own studies ↗
+      </a>
+      <hr />
+      <h3>Why</h3>
+      <p>
+        I want results that are so significant they’re hard to believe, rather
+        than just significant enough to be believable.
+      </p>
+      <p>
+        Peter Thiel says "Brilliant thinking is rare, but courage is in even
+        shorter supply than genius." Some findings might be difficult to accept.
+        This was the case for the smoking studies.
+      </p>
+      <h3>What counts</h3>
       <ul>
         <li>
-          <b>Not too niche.</b> Before getting a driver's license in the US, you
-          need to learn how to coexist with motorcyclists (though they make up
-          only a small minority of motorists). If you want to submit a finding
-          about a niche group, be prepared to explain how they affect the
-          average person.
-        </li>
-        <li>
-          <b>Not obvious.</b> I'm sure your chance of getting hit by a car goes
+          <b>Surprising:</b> I'm sure your chance of getting hit by a car goes
           up 10x if you stand in the middle of the road wearing all black at
           night compared to staying home. Valuable discoveries are unintuitive
           or surprising.
         </li>
+        <li>
+          <b>Relevant:</b> Before getting a driver's license in the US, you need
+          to learn how to coexist with motorcyclists (though they make up only a
+          small minority of motorists). If you want to submit a finding about a
+          niche group, be prepared to explain how they affect the average
+          person.
+        </li>
       </ul>
-      <h3>Why</h3>
-      <p>
-        The goal of this site is to surface important studies and research that
-        might be neglected. Why neglected? Peter Thiel says: "Brilliant thinking
-        is rare, but courage is in even shorter supply than genius." Some
-        findings might be hard to accept, or take time to incorporate into our
-        perception of the world. Studies that show a 10x differences are also
-        more likely to survive the replication crisis. I believe there are many
-        discoveries like the tobacco's link to lung cancer that we're afraid to
-        talk about. These would be important results that can benefit us all.
-      </p>
-      <h3>Get notified with new studies</h3>
+      <hr />
+      <h3>Get notified with new studies / get involved</h3>
       <ConvertKitForm />
       <hr />
       <div style={{ fontSize: "0.75em", textAlign: "center" }}>
--- stuff/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  config: {
-    submitStudyUrl:
-      "https://docs.google.com/forms/d/e/1FAIpQLScaU7yKQl56XHNbw2IzBdpcKP4PtoqyeuoHGuStJnee3pemfg/viewform",
-  },
-};
